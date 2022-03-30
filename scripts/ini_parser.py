5bb02be81afb515e63de38ba1e5d9ffa9c2ae9ed
Christos Synodinos <chris_sinodinos@outlook.com>
2022-03-31 00:05:49 +0300

    docs for .ini file parser script

--- scripts/ini_parser.py
+++ scripts/ini_parser.py
@@ -1,21 +1,39 @@
-import os
+#!/usr/bin/env python3
+from __future__ import annotations
 import configparser
-import psycopg2
-from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
-
 
 class ini_handler:
-    """_summary_
+    """.ini file handler.
 
-    Returns:
-        _type_: _description_
+    Contains 2 methods, one internal parser and a dictionary generator method.
+
+    `.ini file organisation`:
+
+    * [HEADER]
+    * database = a_db_name
+    * pguser = a_username
+    * pgpswd = a_password
+    * pghost = a_host_address
+    * pgport = a_port
     """
 
     def __init__(self, ini):
         self.ini = ini
 
     @staticmethod
-    def __ini_parser(ini):
+    def __ini_parser(ini: str) -> tuple[str, str, str, str, str]:
+        """`Static method`
+
+        Extracts contents of .ini file into a tuple of strings.
+        5 strings are returned for 5 fields.
+
+        Args:
+            * `ini` (_type_: str): .ini file path/name.
+
+        Returns:
+            _type_: tuple: Tuple of strings for every returned value from the .ini file.
+        """
+
         config = configparser.ConfigParser()
         config.read(ini)
         sections = config.sections()
@@ -28,8 +46,16 @@ class ini_handler:
         return database, pguser, pgpswd, pghost, pgport
 
     def _ini_to_dict(self):
-        database, pguser, pgpswd, pghost, pgport= self.__ini_parser(ini = self.ini)
+        """`Instanced method`
 
+        Returns contents of .ini file into a python dictionary.
+        5 fields, database name, username, password, host and port.
+
+        Returns:
+            _type_: dict: The dictionary.
+        """
+
+        database, pguser, pgpswd, pghost, pgport= self.__ini_parser(ini = self.ini)
         info_dict = {'database': database,
                     'pguser': pguser,
                     'pgpswd': pgpswd,
