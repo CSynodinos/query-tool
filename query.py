b57eb1dfcad08d6092cd2968313fefe8f241a4da
Christos Synodinos <chris_sinodinos@outlook.com>
2022-09-07 15:31:37 +0100

    new types and new args

--- query.py
+++ query.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 import argparse, os
 from lib.query_parser import query_tool
 from lib.json_db import json_db, _fl_nm_parser
-from typing import Any
+from typing import Any, Final
 
 def args_parser(msg: str) -> argparse.Namespace:
     """Custom argument parser.
@@ -16,15 +16,20 @@ def args_parser(msg: str) -> argparse.Namespace:
         argparse.Namespace: Namespace of input arguments.
     """
 
-    parser = argparse.ArgumentParser(description = msg, formatter_class = argparse.RawDescriptionHelpFormatter)
-    parser.add_argument("-f", help = "Input file.")
-    parser.add_argument("-p", help = "Pattern to look for. If the file is a txt type file, specify a string pattern. If the file is a .csv or .tsv file, specify a csv file containing the patterns to look for.")
-    parser.add_argument("-o", help = "Optional argument: Output directory for .json file and sqlite3 .db file. Does not work on postgres 4")
-    parser.add_argument("-json", action = 'store_true' , help = "Optional argument: Export into .json file format. Key is either the lines the pattern was found in (for .txt type files) or the columns (for .csv or .tsv files).")
-    parser.add_argument("-db", action = 'store_true', help = "Optional argument: Write the .json output file in a new database when option -json is used. If -pg option is not set, the database will be sqlite3")
-    parser.add_argument("-pg", action = 'store_true', help = "Optional argument: Write the .json output file in a new postgres 4 database when option -json is used.")
-    parser.add_argument("-inf", action = 'store_true', help = "Optional argument: Display information about findings in the stdout.")
-    return parser.parse_args()
+    PARSER: Final[object] = argparse.ArgumentParser(description = msg, formatter_class = argparse.RawDescriptionHelpFormatter)
+    PARSER.add_argument("-f", help = "Input file.")
+    PARSER.add_argument("-p", help = "Pattern to look for. If the file is a txt type file, specify a string pattern. If the file is a .csv or .tsv file, specify a csv file containing the patterns to look for.")
+    PARSER.add_argument("-o", help = "Optional argument: Output directory for .json file and sqlite3 .db file. Does not work on postgres 4")
+    PARSER.add_argument("-json", action = 'store_true' , help = "Optional argument: Export into .json file format. Key is either the lines the pattern was found in (for .txt type files) or the columns (for .csv or .tsv files).")
+    PARSER.add_argument("-jname", help = "Optional argument: If -json flag is set, this flag is used to give a name to the .json output file. Default is None.")
+    PARSER.add_argument("-db", action = 'store_true', help = "Optional argument: Write the .json output file in a new database when option -json is used. If -pg option is not set, the database will be sqlite3")
+
+    PARSER.add_argument("-pg", help = "Optional argument: Write the .json output file in a new postgres 4 database when option -json is used."
+                        "This argument needs the name of the .ini file that has the postgres database information. Check the template.ini file "
+                        "for more info for the .ini organization.")
+
+    PARSER.add_argument("-inf", action = 'store_true', help = "Optional argument: Display information about findings in the stdout.")
+    return PARSER.parse_args()
 
 def bool_parser(var: Any) -> bool:
     """Check if parameter is boolean, if not, convert it to boolean.
@@ -52,46 +57,51 @@ def bool_parser(var: Any) -> bool:
             raise TypeError(f"{var} must be true, True, 1, False, false, 0 or None.")
 
 def main():
-    message = ("\t\t\tRegex Query Tool\n\nReturns a python dictionary with keys being all the lines/columns" 
+    MESSAGE = ("\t\t\tRegex Query Tool\n\nReturns a python dictionary with keys being all the lines/columns" 
     "\nthat a pattern was found and the lines/cells themselves as values.\nSet -json "
     "option as True for writing the dictionary as a .json file.")
 
-    args = args_parser(msg = message)
-    arguments = vars(args)
-    f = arguments.get('f')
-    p = arguments.get('p')
-    o = arguments.get('o')
-    to_json = arguments.get('json')
-    json_to_db = arguments.get('db')
-    json_to_db_pg = arguments.get('pg')
-    info = arguments.get('inf')
+    ARGS_NAMESPACE: argparse.Namespace = args_parser(msg = MESSAGE)
+    ARGUMENTS: dict[str, Any] = vars(ARGS_NAMESPACE)
+    FILE: str | None = ARGUMENTS.get('f')
+    PATTERN: str | None = ARGUMENTS.get('p')
+    OUTPUT: str | None = ARGUMENTS.get('o')
+    JSON: bool = ARGUMENTS.get('json')
+    JSON_NAME: str | None = ARGUMENTS.get('jname')
+    JSON_DB: bool = ARGUMENTS.get('db')
+    JSON_POSTGRES: str | None = ARGUMENTS.get('pg')
+    INFO: bool = ARGUMENTS.get('inf')
 
-    out = query_tool(fl = f, pattern = p).query_wrapper(show_idx = info)
-    if to_json:
+    out: dict = query_tool(fl = FILE, pattern = PATTERN).query_wrapper(show_idx = INFO)
+    if JSON:
+        if not JSON_NAME == None or JSON_NAME == 'None':
+            json_n = _fl_nm_parser(flstr = JSON_NAME, f_type = "json")
+        else:
+            json_n = _fl_nm_parser(flstr = FILE, f_type = "json")
+
         import json
-        json_n = _fl_nm_parser(flstr = f, f_type = "json")
-        if o:
-            if os.path.isdir(o):
-                json_n = os.path.join(o, json_n)
+        if OUTPUT:
+            if os.path.isdir(OUTPUT):
+                json_n = os.path.join(OUTPUT, json_n)
             else:
-                os.makedirs(o)
-                json_n = os.path.join(o, json_n)
+                os.makedirs(OUTPUT)
+                json_n = os.path.join(OUTPUT, json_n)
 
         with open(json_n, 'w') as json_file:
             json.dump(out, json_file, default = lambda o: o.__dict__, sort_keys = True, indent = 2)
 
-        if json_to_db:
-            if json_to_db_pg:
+        if JSON_DB:
+            if not JSON_POSTGRES == None or JSON_POSTGRES == 'None':
                 print('Insertion of .json keys and values into a postgres4 database.')
-                output = json_db(db_type = 'postgres', jsonf = json_n).invoker(out = o)
+                output = json_db(db_type = 'postgres', jsonf = json_n, ini = JSON_POSTGRES).invoker(out = OUTPUT)
                 print(f'Operation Complete! Data parsed into the {output} database.')
             else:
                 print('Insertion of .json keys and values into a sqlite3 database.')
-                json_db(db_type = 'sqlite', jsonf = json_n).invoker(out = o)
+                json_db(db_type = 'sqlite', jsonf = json_n).invoker(out = OUTPUT)
                 print('Operation Complete!')
 
-    if not to_json:
-        if json_to_db:
+    if not JSON:
+        if JSON_DB:
             raise RuntimeError("Option -db was used without setting option -json as True.")
 
 if __name__ == "__main__":
